3047588dbd0172b92ca09a1afc3c89954dab5dce
Jeniffer Souza Ribeiro <127236771+JenifferSR@users.noreply.github.com>
2023-04-24 14:33:55 -0300

    Update PostagemRepository.java

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -6,12 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
-
-<<<<<<< HEAD
-
-=======
 @Repository
->>>>>>> 747edc1be38d070294b311936ebf51ade2515650
+
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
 	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
